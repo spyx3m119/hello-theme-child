146e6c49341fabc35801978d4117c5b3a56916a5
Blade <blademp_119@yahoo.com>
2023-09-29 11:17:18 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -21,7 +21,12 @@ function hello_elementor_child_enqueue_scripts() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
+if (isset($_GET['developer'])) {
+    $showDev = $_GET['developer'];
+} else {
+    $showDev = '';
+}
 
-if($_GET['developer'] == 'yes'){
-	echo '<h3 id="sitedev" style="position: fixed; z-index: 999999; width: 100%; height: 100%; text-align: center; top: 0; color: #95A5A6; background: rgba(0,0,0,0.6);"><span style="background: #ECF0F1; padding: 50px; position: absolute; top: 50%; transform: translate(-50%, -50%);"><small>Design and Developed by:</small> <a href="vladparole.com"><u>Vladimir M. Parole</u></a></span></h3>';
+if($showDev == 'yes'){
+	echo "<h3 id='sitedev' style='position: fixed; z-index: 999999; width: 100%; height: 100%; text-align: center; top: 0; color: #95A5A6; background: rgba(0,0,0,0.6);'><span style='background: #ECF0F1; padding: 50px; position: absolute; top: 50%; transform: translate(-50%, -50%);'><small>Design and Developed by:</small> <a href='vladparole.com'><u>Vladimir M. Parole</u></a></span></h3>";
 }
